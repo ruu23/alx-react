268058c76c26735ccd04ea296692e28a4bf50e9e
ruu23 <arwamah777@gmail.com>
2024-10-31 04:33:11 +0300

    deply the application

---
+++ 0x01-react_intro/task_4/dashboard/src/utils.js
@@ -0,0 +1,18 @@
+export function getFullYear() {
+  const currentYear = new Date().getFullYear();
+  return currentYear;
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex) {
+    return "Holberton School"
+  } else {
+    return "Holberton School main dashboard"
+  }
+}
+
+export function getLatestNotification() {
+  return '<strong>Urgent requirement</strong> - complete by EOD'
+}
+
+ 
