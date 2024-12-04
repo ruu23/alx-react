938425d8c67ffc8cfe567f7bf1218fe34e058443
ruu23 <arwamah777@gmail.com>
2024-12-04 22:30:08 +0200

    use fromJS

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,13 @@
+import { fromJS } from "immutable";
+
+export default function getImmutableObject(object) {
+  return fromJS(object)
+}
+const obj = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132
+};
+
+console.log(getImmutableObject(obj));
